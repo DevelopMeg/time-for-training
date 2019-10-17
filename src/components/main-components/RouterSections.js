b52746260343e27fce4777675e1b0b97c1043758
Asia <asia.dobrowolska95@gmail.com>
2019-10-17 20:25:14 +0200

    create route component - RouterSections

--- src/components/main-components/RouterSections.js
+++ src/components/main-components/RouterSections.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "../router-components/HomePage";
+import TrainingList from "../router-components/TrainingList";
+import PriceList from "../router-components/PriceList";
+import TrainersInfo from "../router-components/TrainersInfo";
+import ErrorPage from "../router-components/ErrorPage";
 
 class RouterSections extends Component {
   render() {
-    return <Switch></Switch>;
+    return (
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/training-list" component={TrainingList} />
+        <Route path="/price-list" component={PriceList} />
+        <Route path="/trainers-info" component={TrainersInfo} />
+        <Route component={ErrorPage} />
+      </Switch>
+    );
   }
 }
 
