7008ec25ad3501b66ca4d3fdbddf93ceaa84d576
Asia <asia.dobrowolska95@gmail.com>
2019-11-25 10:38:40 +0100

    add functions change class - animating item menu

--- src/components/main-components/Header.js
+++ src/components/main-components/Header.js
@@ -2,17 +2,60 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 class Header extends Component {
+  state = {
+    nameNavItem: "",
+    classNameDecorateDot: ""
+  };
+
   pagesList = [
     { name: "training-list", path: "/training-list" },
     { name: "price-list", path: "/price-list" },
     { name: "trainers-info", path: "/trainers-info" }
   ];
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.nameNavItem !== this.state.nameNavItem) {
+      this.pagesList.forEach(page => {
+        if (this.state.nameNavItem === page.name) {
+          this.setState({
+            classNameDecorateDot: `navigation__decorate-dot--${page.name}`
+          });
+        }
+      });
+    }
+  }
+
+  removeMoveDecorateDot = () => {
+    this.setState({
+      classNameDecorateDot: ""
+    });
+  };
+
+  handleAddNameNavItem = e => {
+    this.setState({
+      nameNavItem: e.target.innerHTML
+    });
+  };
+
+  handleRemoveNameNavItem = () => {
+    this.setState({
+      nameNavItem: ""
+    });
+  };
+
   render() {
     const menu = this.pagesList.map((page, i) => {
       return (
         <NavLink to={page.path} key={i} className="navigation__link">
-          <li className={`navigation__item navigation__item--${page.name}`}>
+          <li
+            className={`navigation__item navigation__item--${page.name}`}
+            onClick={this.props.handleNav}
+            onMouseOver={this.handleAddNameNavItem}
+            onMouseOut={() => {
+              this.handleRemoveNameNavItem();
+              this.removeMoveDecorateDot();
+            }}
+          >
             {page.name}
           </li>
         </NavLink>
@@ -21,19 +64,37 @@ class Header extends Component {
 
     return (
       <header className="head">
-        <NavLink to="/" exact>
+        <NavLink to="/" exact style={{ textDecoration: "none" }}>
           <h2 className="head__title">time for training</h2>
         </NavLink>
 
         <nav className="menu">
-          <button className="hamburger">
+          <button
+            className={
+              this.props.navActive ? "hamburger hamburger--active" : "hamburger"
+            }
+            onClick={this.props.handleNav}
+          >
             <span className="hamburger__box">
               <span className="hamburger__inner"></span>
             </span>
           </button>
 
-          <div className="navigation">
-            <div className="navigation__decorate-dot"></div>
+          <div
+            className={
+              this.props.navActive
+                ? "navigation"
+                : "navigation navigation--close"
+            }
+          >
+            <div
+              className={
+                this.state.classNameDecorateDot !== ""
+                  ? `navigation__decorate-dot ${this.state.classNameDecorateDot}`
+                  : "navigation__decorate-dot"
+              }
+            ></div>
+
             <ul className="navigation__item-list">{menu}</ul>
           </div>
         </nav>
