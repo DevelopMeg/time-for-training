1dfbae89f8199e079e10c45a17d12eb1982160a4
Asia <asia.dobrowolska95@gmail.com>
2019-11-25 10:37:58 +0100

    add function handle nav

--- src/components/main-components/App.js
+++ src/components/main-components/App.js
@@ -3,19 +3,39 @@ import { BrowserRouter } from "react-router-dom";
 import RouterSections from "./RouterSections";
 import Header from "./Header";
 import Footer from "./Footer";
+import "../../sass/style.scss";
 
 class App extends Component {
+  state = {
+    navActive: false
+  };
+
+  handleNav = () => {
+    this.setState(prevState => ({
+      navActive: !prevState.navActive
+    }));
+  };
+
+  componentDidUpdate() {
+    if (this.state.navActive && window.innerWidth < 1024) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "scroll";
+    }
+  }
+
   render() {
     return (
       <div className="wrapper">
         <BrowserRouter>
-          <Header />
-          <main className="main">{<RouterSections />}</main>
+          <Header handleNav={this.handleNav} navActive={this.state.navActive} />
+          <main className={this.state.navActive ? "main--active" : "main"}>
+            {<RouterSections />}
+          </main>
           <Footer />
         </BrowserRouter>
       </div>
     );
   }
 }
-
 export default App;
