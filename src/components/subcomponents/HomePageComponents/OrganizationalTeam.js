75eab6eb4f426c7349e9b4b255a7bcb45af45d58
Asia <asia.dobrowolska95@gmail.com>
2019-12-06 17:10:48 +0100

    get data team - fetch

--- src/components/subcomponents/HomePageComponents/OrganizationalTeam.js
+++ src/components/subcomponents/HomePageComponents/OrganizationalTeam.js
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
+import OrganizationalPerson from "./OrganizationalPerson";
+import ErrorData from "../../router-components/ErrorData";
 
-class OrganizationalTeam extends Component {
+class OrganizatiolTeam extends Component {
+  state = {
+    dataTeam: [],
+    errorDataTeam: false
+  };
+  componentDidMount() {
+    this.handleGetTeam();
+  }
+
+  handleGetTeam = async () => {
+    const APITeam = "https://randomuser.me/api/?results=8&nat=US&gender=female";
+
+    try {
+      const response = await fetch(APITeam);
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      return this.setState({
+        dataTeam: data.results
+      });
+    } catch (err) {
+      return this.setState({
+        errorDataTeam: true
+      });
+    }
+  };
+
   render() {
+    if (this.state.errorDataTeam) {
+      return <ErrorData />;
+    }
+
     return (
       <section className="section-organizational-team">
         <div className="section-organizational-team__box-title">
@@ -14,4 +49,4 @@ class OrganizationalTeam extends Component {
   }
 }
 
-export default OrganizationalTeam;
+export default OrganizatiolTeam;
