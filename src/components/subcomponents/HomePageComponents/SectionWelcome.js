c35596dc6b61f0b5273f22f6c5d8e14981849554
Asia <asia.dobrowolska95@gmail.com>
2019-12-06 17:14:05 +0100

    add animation SectionWelcome - arrow scroll down

--- src/components/subcomponents/HomePageComponents/SectionWelcome.js
+++ src/components/subcomponents/HomePageComponents/SectionWelcome.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SectionWelcome = () => {
-  return (
-    <section className="section-welcome">
-      <div className="section-welcome__box-title">
-        <h1 className="section-welcome__title">Welcome to Time For Training</h1>
-        <h2 className="section-welcome__subtitle">changes start with you</h2>
-      </div>
-      <div className="scroll-down__box">
-        <i className="fas fa-angle-double-down scroll-down__sign"></i>
-      </div>
-    </section>
-  );
-};
+class SectionWelcome extends Component {
+  constructor(props) {
+    super(props);
+    this.sctWelcomeRef = React.createRef();
+  }
+
+  handleScrollDown = () => {
+    window.scrollTo({
+      behavior: "smooth",
+      top:
+        this.sctWelcomeRef.current.offsetTop +
+        this.sctWelcomeRef.current.offsetHeight
+    });
+  };
+
+  render() {
+    return (
+      <section className="section-welcome" ref={this.sctWelcomeRef}>
+        <div className="section-welcome__box-title">
+          <h1 className="section-welcome__title">
+            Welcome to Time For Training
+          </h1>
+          <h2 className="section-welcome__subtitle">changes start with you</h2>
+        </div>
+        <div className="scroll-down__box" onClick={this.handleScrollDown}>
+          <i className="fas fa-angle-double-down scroll-down__sign"></i>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default SectionWelcome;
