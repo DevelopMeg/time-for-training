288767666dd83ab61157c717873d115fa3dcb9a2
Asia <asia.dobrowolska95@gmail.com>
2019-12-09 15:19:23 +0100

    add get trainers - TrainersInfo

--- src/components/router-components/TrainersInfo.js
+++ src/components/router-components/TrainersInfo.js
@@ -1,13 +1,14 @@
 import React from "react";
 import purposeTraining from "../../image/purposeTraining.jpg";
 
-const TrainersInfo = () => {
+const TrainersInfo = props => {
   return (
     <section className="section-trainers-info">
       <div className="section-trainers-info__box-title">
         <h2 className="section-trainers-info__title">Trainers Info</h2>
       </div>
       <div className="trainers">
+        {props.listTrainers}
         <div className="trainers__image-fixed"></div>
       </div>
       <img
