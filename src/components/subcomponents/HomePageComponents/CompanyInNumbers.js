35b41368f266a575156f2a870d9384808418b58f
Asia <asia.dobrowolska95@gmail.com>
2019-12-09 15:07:08 +0100

    add animation - company in number

--- src/components/subcomponents/HomePageComponents/CompanyInNumbers.js
+++ src/components/subcomponents/HomePageComponents/CompanyInNumbers.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
 class CompanyInNumbers extends Component {
-  state = {
-    numberTraining: 0,
-    numberClient: 0,
-    numberTrainers: 0,
-    numberTeam: 0
-  };
+  constructor(props) {
+    super(props);
+    this.countUp = React.createRef();
+    this.state = {
+      numberTraining: 0,
+      numberClient: 0,
+      numberTrainers: 0,
+      numberTeam: 0
+    };
+  }
 
   dataCompanyNumber = [
     { type: "numberTraining", number: 50, title: "training" },
@@ -15,6 +19,67 @@ class CompanyInNumbers extends Component {
     { type: "numberTeam", number: 8, title: "organize team" }
   ];
 
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (this.countUp.current !== null) {
+      const companyNumberTop = this.countUp.current.offsetTop;
+      const companyNumberHeight = this.countUp.current.offsetHeight;
+
+      if (
+        window.scrollY >=
+        companyNumberTop + companyNumberHeight - window.innerHeight + 80
+      ) {
+        this.handleAddNumber();
+      }
+    }
+  };
+
+  handleAddNumber = () => {
+    const duration = 5000;
+
+    this.dataCompanyNumber.forEach(item => {
+      const addNumberOne = () => {
+        if (item.type === "numberTraining") {
+          if (item.number !== this.state.numberTraining) {
+            this.setState(prevState => ({
+              numberTraining: prevState.numberTraining + 1
+            }));
+          } else {
+            clearInterval(idInterval);
+          }
+        } else if (item.type === "numberClient") {
+          if (item.number !== this.state.numberClient) {
+            this.setState(prevState => ({
+              numberClient: prevState.numberClient + 1
+            }));
+          } else {
+            clearInterval(idInterval);
+          }
+        } else if (item.type === "numberTrainers") {
+          if (item.number !== this.state.numberTrainers) {
+            this.setState(prevState => ({
+              numberTrainers: prevState.numberTrainers + 1
+            }));
+          } else {
+            clearInterval(idInterval);
+          }
+        } else if (item.type === "numberTeam") {
+          if (item.number !== this.state.numberTeam) {
+            this.setState(prevState => ({
+              numberTeam: prevState.numberTeam + 1
+            }));
+          } else {
+            clearInterval(idInterval);
+          }
+        }
+      };
+
+      const idInterval = setInterval(addNumberOne, duration / item.number);
+    });
+  };
   render() {
     const {
       numberTraining,
@@ -42,7 +107,11 @@ class CompanyInNumbers extends Component {
       );
     });
 
-    return <aside className="company-in-number">{companyNumber}</aside>;
+    return (
+      <aside className="company-in-number" ref={this.countUp}>
+        {companyNumber}
+      </aside>
+    );
   }
 }
 
